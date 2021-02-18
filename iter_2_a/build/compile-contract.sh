8aef2dbf421c7b66d1c6932d561b91d0c2ef202d
Daniel Jozsef <daniel.jozsef@gmail.com>
2021-02-18 15:04:53 +0100

    Minor script fix

--- iter_2_a/build/compile-contract.sh
+++ iter_2_a/build/compile-contract.sh
@@ -6,5 +6,8 @@ echo "Eg. ./compile-contact tmp/main.mligo"
 exit 1
 fi
 
+filename=${1##*/}
+basename="${filename%.[^.]*}"
+
 mkdir -p bin
-ligo compile-contract ${1} main > bin/${1}.tz
+ligo compile-contract ${1} main > bin/${basename}.tz
