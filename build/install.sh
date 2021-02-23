01c28c317995c5751e8e50d202e5b346d3879ca7
Daniel Jozsef <daniel.jozsef@gmail.com>
2021-02-23 18:42:17 +0100

    Install script added

---
+++ build/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+lazy_folder="./bin/lazy"
+hex_files=${lazy_folder}/*.hex
+
+echo "Installing contract..."
+tezos-client originate contract snorlax transferring 0 from alice \
+	running bin/main.tz --init "`cat bin/storage.tz`" --burn-cap 0.5 --force
+
+for f in ${hex_files}
+do
+	filename=${f##*/}
+	filename_without_extension=${filename%.[^.]*}
+	lambda_name=${filename_without_extension#*.}
+	echo "Installing ${filename} as ${lambda_name}"
+	tezos-client transfer 0 from alice to snorlax --burn-cap 0.5 \
+		--arg \(Right\(Left\(Left\(Right\(Pair\ \"${lambda_name}\"\ `cat ${f}`\)\)\)\)\)
+done
--- build/install_lambda.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-tezos-client transfer 0 from alice to lazytest --burn-cap 0.5 --arg \(Right\(Left\(Left\(Right\(Pair\ \"${1}\"\ ${2}\)\)\)\)\)\
-
