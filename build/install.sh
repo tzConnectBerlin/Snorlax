f720f7e45189c493c4845bdbe5062313af6487b1
Daniel Jozsef <daniel.jozsef@gmail.com>
2021-02-23 19:55:17 +0100

    Some install stuff

--- build/install.sh
+++ build/install.sh
@@ -12,9 +12,10 @@ do
 	filename=${f##*/}
 	filename_without_extension=${filename%.[^.]*}
 	lambda_name=${filename_without_extension#*.}
-	echo "Installing ${filename} as ${lambda_name}"
+	echo "Installing ${filename} as ${lambda_name}..."
 	tezos-client transfer 0 from alice to snorlax --burn-cap 0.5 \
 		--entrypoint installLambda --arg \(Pair\ \"${lambda_name}\"\ `cat ${f}`\)
 done
 
+echo "Sealing contract..."
 tezos-client transfer 0 from alice to snorlax --burn-cap 0.5 --entrypoint sealContract
